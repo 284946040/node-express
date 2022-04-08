22051f40cf7b4fe65f203aa91520d79f11e0cb70
284946040 <284946040@qq.com>
2022-04-08 16:27:56 +0800

    00999

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server0101020!');
+  res.send('Hello from the server0101020!devops');
 });
 
 app.get('/health', (req, res) => {
